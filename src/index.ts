24a9ee5f2b884bac5ca5e630f6744eec112b30e3
Choicelin <413807584@qq.com>
2020-09-12 17:42:30 +0800

    0.0.1

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export const uuid: (length: number) => string = (length) => {
+  const s: string[] = []
+  const randomStr: string = '0123456789abcdef'
+  for (let i = 0; i < length; i++) {
+    s[i] = randomStr.substr(Math.floor(Math.random() * 0x10), 1)
+  }
+  s[14] = '4'
+  s[19] = randomStr.substr((+s[19] & 0x3) | 0x8, 1)
+  s[8] = s[13] = s[18] = s[23] = '-'
+  return s.join('')
+}
